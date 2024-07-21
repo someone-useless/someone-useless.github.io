a8c7b866ed76b0de3c9ae54f224e5eac65f88751
onelone852 <71179051+onelone852@users.noreply.github.com>
2024-07-21 12:33:54 +0000

    Deploying to gh-pages from @ someone-useless/someone-useless.github.io@bdbd71f7dd273fe5bc7fbc80a3e6e9537f257066 🚀

--- _build/assets/about-CAuQfI6G.js
+++
@@ -1,1 +0,0 @@
-import{p as n,C as i,w as l,j as o,y as s,T as m}from"./index-Bwyft0bN.js";var p=s("<main><!$><!/><h1>About");function $(){return(()=>{var e=n(p),r=e.firstChild,[t,a]=i(r.nextSibling);return t.nextSibling,l(e,o(m,{children:"About"}),t,a),e})()}export{$ as default};
---
+++ _build/assets/index-VJpWUJKW.js
@@ -0,0 +1,1 @@
+import{p as e,y as a}from"./web-D3OS7vh0.js";var t=a("<div id=page><a href=/ id=back-button>BACK TO THE HELL</a><h1>STACK 10!</h1><br><a href=./rules class=subpage>Rules</a><br><a href=./game class=subpage>Game");function s(){return e(t)}export{s as default};
